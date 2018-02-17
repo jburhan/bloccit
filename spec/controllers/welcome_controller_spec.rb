2d8e457a5f2bd1bf4ba350a5a4f1616bfbaf25f4
jburhan <jimburhan@gmail.com>
2018-02-17 15:53:19 -0800

    testing-assignment completed

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -18,5 +18,12 @@ RSpec.describe WelcomeController, type: :controller do
       end
     end
 
+    describe "GET faq" do
+      it "renders the faq page template" do
+        get :faq
+        expect(response).to render_template("faq")
+      end
+    end
+
   end
 end
