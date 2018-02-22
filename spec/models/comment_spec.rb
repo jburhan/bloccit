bf9fea53f27c99d63b7704a6528c77369dee2660
jburhan <jimburhan@gmail.com>
2018-02-22 12:22:41 -0800

    bloccit-models-assignment completed

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  let(:post) { Post.create!(title: "New Post Title", body: "New Post Body")}
-  let(:comment) { Comment.create!(body: "Comment Body", post: post)}
+  let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
+  let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
 
   describe "attributes" do
     it "has a body attribute" do
