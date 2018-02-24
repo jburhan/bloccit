73ad679f95073ebde17970d233cdabc5ad68e8f8
jburhan <jimburhan@gmail.com>
2018-02-24 00:56:07 -0800

    bloccit-seeding completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'random_data'
+
+#(number).times will allow code to run as many times as the number
+50.times do
+  #having ! after a method, would instruct the method
+  #to raise a problem with the data we're seeding.
+  Post.create!(
+    # RandomData method will be created by ourself to create
+    # random strings for title and body. This is known as "wishful coding"
+    # this allow us to stay focused on one problem at a time.
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+
+  )
+end
+# we call sample on the array returned by Post.all, in order to pick a random post
+# to associate each comment with. sample returns a random element
+post = Post.all
+
+100.times do
+  Comment.create!(
+    post: post.sample,
+    body: RandomData.random_paragraph
+
+  )
+end
+
+puts "Seed finished"
+puts "#{Post.count} post created"
+puts "#{Comment.count} comments created"
