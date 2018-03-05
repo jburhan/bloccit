809a556ea6022e909c570add320eaf522048e75b
jburhan <jimburhan@gmail.com>
2018-03-04 18:22:35 -0800

    bloccit-crud2 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
+  # makes comments dependent on posts existence
+  # when posts are deleted the comments are deleted as well
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 RSpec.describe PostsController, type: :controller do
   let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
-  describe "Post index" do
+  describe "Get index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
@@ -17,7 +17,7 @@ RSpec.describe PostsController, type: :controller do
   end
 
 # when new is invoked a new and unsaved post object is created
-  describe "Post new" do
+  describe "Get new" do
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
@@ -53,7 +53,7 @@ RSpec.describe PostsController, type: :controller do
 
   end
 
-  describe "Post show" do
+  describe "Get show" do
     it "returns http success" do
       # we pass id as a parameter
       get :show, params: { id: my_post.id }
@@ -73,12 +73,67 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-  #
-  # describe "GET #edit" do
-  #   it "returns http success" do
-  #     get :edit
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET edit" do
+    it "returns http success" do
+      get :edit, params: { id: my_post.id}
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #edit view" do
+      get :edit, params: { id: my_post.id}
+      expect(response).to render_template :edit
+    end
+
+    it "assigns post to be updated to @post" do
+      get :edit, params: { id:my_post.id }
+
+      post_instance = assigns(:post)
+
+      expect(post_instance.id).to eq my_post.id
+      expect(post_instance.title).to eq my_post.title
+      expect(post_instance.body).to eq my_post.body
+
+    end
+
+  end
+
+  describe "PUT update" do
+    it "updates post with expected attributes" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+
+      put :update, params: { id: my_post.id, post: {title: new_title, body: new_body}}
+
+      updated_post = assigns(:post)
+      expect(updated_post.id).to eq my_post.id
+      expect(updated_post.title).to eq new_title
+      expect(updated_post.body).to eq new_body
+    end
+
+    it "redirects to the updated post" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+
+      put :update, params: {id: my_post.id, post: {title: new_title, body: new_body}}
+      expect(response).to redirect_to my_post
+    end
+  end
+
+  describe "DELETE destroy" do
+
+    it "deletes the post" do
+      delete :destroy, params: { id: my_post.id }
+      # finds post by id and make sure method removes it by expecting 0 count
+      count = Post.where({id: my_post.id}).size
+      expect(count).to eq 0
+    end
+
+    it "redirects to posts index" do
+      delete :destroy, params: { id: my_post.id}
+
+      expect(response).to redirect_to posts_path
+    end
+
+  end
 
 end
